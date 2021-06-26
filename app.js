ad8c66aba4ae3aaaa178f8783e072a5255a79a3a
Sanskar Jethi <sansyrox@gmail.com>
2021-06-26 15:51:27 +0530

    Fix Google app engine deployment

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
 
 const middleware = require("./utils/middleware");
 
@@ -10,4 +12,14 @@ require("./routes/main")(app);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-module.exports = app;
+require("./conference")(server);
+
+if (module === require.main) {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log("Press Ctrl+C to quit.");
+  });
+}
+
+module.exports = server;
